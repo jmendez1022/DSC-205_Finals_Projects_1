0f26a2989588d183a7ab2a0ffaf49f6b1ad98dba
jmendez1022 <mendezzavaj1@southernct.edu>
2024-11-21 16:07:18 -0500

    Update FINAL_PROJECT.py

--- FINAL_PROJECT.py
+++ FINAL_PROJECT.py
@@ -2,8 +2,6 @@
 import numpy as np
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.dates import DateFormatter
 import folium
 
 URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSNe3p3E6MOF-F2uzAucnPmOaYJsH9iBTa7NXrbSSvIbZj4LEo5v5NboBT_14sbrsrNRnwPIRPZLcQ7/pub?output=csv"
